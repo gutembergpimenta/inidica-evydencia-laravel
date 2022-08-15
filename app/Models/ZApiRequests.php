fc20a96b1fffb3e9b4a204432dbd0afea917329c
Gutemberg Pimenta <gutem14@gmail.com>
2022-08-15 17:11:56 -0300

    ...

--- app/Models/ZApiRequests.php
+++ app/Models/ZApiRequests.php
@@ -164,7 +164,7 @@ class ZApiRequests extends Model
 
         curl_setopt_array(
             $curl, array(
-                $url."chats?pageSize=99999",
+                CURLOPT_URL => $url."chats?pageSize=99999",
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
